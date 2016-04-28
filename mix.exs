8332276b7a58c4c3b2ccdbbca6c62b2b8615476a
Gizeta <0w0@gizeta.tk>
2016-04-29 00:00:11 +0900

    bump v0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule KcspEx.Mixfile do
 
   def project do
     [app: :kcsp_ex,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
